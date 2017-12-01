e95059999e34ae687d61e6bd35cf8394823a1478
Samuel Silvester <pighead10@hogpog.co.uk>
2017-12-01 17:52:37 +0000

    fix worldmanager

--- GameGig2017/WorldManager.cpp
+++ GameGig2017/WorldManager.cpp
@@ -17,7 +17,7 @@ WorldManager::~WorldManager() {
 }
 
 void WorldManager::update(int frame_time) {
-	auto& result = entities_.find(timer_);
+	auto result = entities_.find(timer_);
 	
 	if (result != entities_.end()) {
 		for (auto& it : result->second) {
